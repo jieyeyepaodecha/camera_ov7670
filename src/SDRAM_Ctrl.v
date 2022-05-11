d25136e81d2840fe91322817d78490e5c0c7c6d7
LiuJJ <2365126855@qq.com>
2022-05-11 21:56:49 +0800

    src

--- src/SDRAM_Ctrl.v
+++ src/SDRAM_Ctrl.v
@@ -28,12 +28,13 @@ parameter   WRITE= 4'b0100;
 parameter	READ = 4'b1000;
 
 //FSM_INIT Steps
-parameter	IDLE_INIT = 6'b00_0001;
-parameter	WAIT_INIT = 6'b00_0010;
-parameter	PR_INIT   = 6'b00_0100;
-parameter	AR_INIT0  = 6'b00_1000;
-parameter	AR_INIT1  = 6'b01_0000;
-parameter	LMR_INIT  = 6'b10_0000;
+parameter	IDLE_INIT  = 6'b00_0001;
+parameter	WAIT_INIT  = 6'b00_0010;
+parameter	PR_INIT    = 6'b00_0100;
+parameter	AR_INIT0   = 6'b00_1000;
+parameter	AR_INIT1   = 6'b01_0000;
+parameter	LMR_INIT   = 6'b10_0000;
+parameter   DELAY_INIT = 6'b00_0000;
 
 //FSM_IDLE Steps
 parameter	PR_IDLE   = 5'b0_0001;
@@ -60,20 +61,55 @@ parameter   tRCD   = 3;//20ns
 parameter   tMRD   = 2;//2CLK
 parameter   tDAL   = 5;//2CLK + 20ns
 
+//single flag
+reg SDRAM_flag = 1'b1;//SDRAM start flag
+reg wait_flag;
+reg delay_flag, delay_finish;
+reg pr_flag;
+reg ar0_flag, ar1_flag;
+
 //time_cnt
-reg cnt_flag;
 reg [13:0]  time_cnt;
 
 always @(posedge sclk or negedge rst_n)
     if(!rst_n)
         time_cnt <= 'd0;
-    else if(cnt_flag == 1'b1)
+    else if(delay_flag == 1'b1)
         time_cnt <= time_cnt + 1'b1;
     else
         time_cnt <= 'd0;
 
+//delay
+always @(posedge sclk or negedge rst_n)
+    if(!rst_n)
+        delay_finish <= 1'b0;
+    else if(time_cnt == delay_select)
+        delay_finish <= 1'b1;
+    else
+        delay_finish <= 1'b0;
+
+//single pin
+reg [4:0]   single;
+
+always @(*) 
+    {CKE,CS,RAS,CAS,WE} = single;
+
+always @(posedge sclk or negedge rst_n)
+    if(!rst_n)
+        single <= NOP;
+    else if(delay_flag == 1'b1)
+        single <= NOP;
+    else if(wait_flag == 1'b1)
+        single <= NOP;
+    else if(pr_flag == 1'b1)
+        single <= PR;
+    else if(ar0_flag == 1'b1)
+        single <= AR_INIT0;
+    else if(ar1_flag == 1'b1)
+        single <= AR_INIT1;
+
 //FSM
-reg SDRAM_flag = 1'b1;//SDRAM start flag
+reg [13:0]  delay_select;
 reg state, next_state;
 
 always @(posedge sclk or negedge rst_n)
@@ -82,7 +118,6 @@ always @(posedge sclk or negedge rst_n)
     else
         state <= next_state;
 
-//FSM_INIT
 always @(posedge sclk or negedge rst_n)
     if(!rst_n)
         next_state <= IDLE_INIT;
@@ -93,6 +128,27 @@ always @(posedge sclk or negedge rst_n)
             else    next_state <= IDLE_INIT;
         end
         WAIT_INIT:begin
+            next_state <= DELAY_INIT;
+        end
+        PR_INIT:begin
+            next_state <= DELAY_INIT;
+        end
+        AR_INIT0:begin
+            next_state <= DELAY_INIT;
+        end
+        AR_INIT1:begin
+            next_state <= DELAY_INIT;
+        end
+        LMR_INIT:begin
+            next_state <= DELAY_INIT;
+        end
+        DELAY_INIT:begin
+            if(delay_finish == 1'b1 && wait_flag == 1'b1)   next_state <= PR_INIT;
+            else if(delay_finish == 1'b1 && pr_flag == 1'b1)    next_state <= AR_INIT0;
+            else if(delay_finish == 1'b1 && ar0_flag == 1'b1)    next_state <= AR_INIT1;
+            else if(delay_finish == 1'b1 && ar1_flag == 1'b1)    next_state <= LMR_INIT;
+        end
+        default:begin
             
         end
 
@@ -100,19 +156,60 @@ always @(posedge sclk or negedge rst_n)
 
 always @(posedge sclk or negedge rst_n)
     if(!rst_n)begin
-        ;
+        delay_flag <= 1'b0;
+        delay_select <= 'd0;
+        wait_flag <= 1'b0;
+        pr_flag <= 1'b0;
     end
     else
         case(state)
         IDLE_INIT:begin
-            ;
+            delay_flag <= 1'b0;
+            delay_select <= 'd0;
+            wait_flag <= 1'b0;
+            pr_flag <= 1'b0;
         end
         WAIT_INIT:begin
-            ;
+            delay_flag <= 1'b0;
+            delay_select <= _100us;
+            wait_flag <= 1'b1;
+            pr_flag <= 1'b0;
         end
-
+        PR_INIT:begin
+            delay_flag <= 1'b0;
+            delay_select <= tRP;
+            wait_flag <= 1'b0;
+            pr_flag <= 1'b1;
+        end
+        AR_INIT0:begin
+            delay_flag <= 1'b0;
+            delay_select <= tRRC;
+            wait_flag <= 1'b0;
+            pr_flag <= 1'b0;
+        end
+        AR_INIT1:begin
+            delay_flag <= 1'b0;
+            delay_select <= tRRC;
+            wait_flag <= 1'b0;
+            pr_flag <= 1'b0;
+        end
+        LMR_INIT:begin
+            delay_flag <= 1'b0;
+            delay_select <= tMRD;
+            wait_flag <= 1'b0;
+            pr_flag <= 1'b0;
+        end
+        DELAY_INIT:begin
+            delay_flag <= 1'b1;
+            delay_select <= delay_select;
+            wait_flag <= wait_flag;
+            pr_flag <= pr_flag;
+        end
         default:begin
-            ;
+            delay_flag <= 1'b0;
+            delay_select <= 'd0;
+            wait_flag <= 1'b0;
+            pr_flag <= 1'b0;
         end
         endcase
 
