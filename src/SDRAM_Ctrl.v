3a3d9e4d43952f09f6e809b07458db071f1e37d6
LiuJJ <2365126855@qq.com>
2022-05-11 19:00:07 +0800

    src

--- src/SDRAM_Ctrl.v
+++ src/SDRAM_Ctrl.v
@@ -2,7 +2,7 @@
 //Mode Register BA1 BA0 A12~A0: 15'b000_0000_0010_0000
 
 module SDRAM_Ctrl(
-input	wire	sclk,
+input	wire	sclk,//133MHz 7.5ns
 input	wire	rst_n,
 output	reg		CKE,
 output	reg		CS,//芯片选择
@@ -28,11 +28,12 @@ parameter   WRITE= 4'b0100;
 parameter	READ = 4'b1000;
 
 //FSM_INIT Steps
-parameter	WAIT_INIT = 5'b0_0001;
-parameter	PR_INIT   = 5'b0_0010;
-parameter	AR_INIT0  = 5'b0_0100;
-parameter	AR_INIT1  = 5'b0_1000;
-parameter	LMR_INIT  = 5'b1_0000;
+parameter	IDLE_INIT = 6'b00_0001;
+parameter	WAIT_INIT = 6'b00_0010;
+parameter	PR_INIT   = 6'b00_0100;
+parameter	AR_INIT0  = 6'b00_1000;
+parameter	AR_INIT1  = 6'b01_0000;
+parameter	LMR_INIT  = 6'b10_0000;
 
 //FSM_IDLE Steps
 parameter	PR_IDLE   = 5'b0_0001;
@@ -50,5 +51,70 @@ parameter	WR_WRITE1  = 3'b100;
 parameter	ACT_READ   = 3'b001;
 parameter	RD_READ    = 3'b010;
 parameter	DATA_READ  = 3'b100;
+
+//command time interval
+parameter   _100us = 13333;
+parameter   tRP    = 3;
+parameter   tRRC   = 9;
+parameter   tRCD   = 3;
+parameter   tMRD   = 2;
+parameter   tDAL   = 5;
+
+//time_cnt
+reg cnt_flag;
+reg [13:0]  time_cnt;
+
+always @(posedge sclk or negedge rst_n)
+    if(!rst_n)
+        time_cnt <= 'd0;
+    else if(cnt_flag == 1'b1)
+        time_cnt <= time_cnt + 1'b1;
+    else
+        time_cnt <= 'd0;
+
+//FSM
+reg SDRAM_flag = 1'b1;//SDRAM start flag
+reg state, next_state;
+
+always @(posedge sclk or negedge rst_n)
+    if(!rst_n)
+        state <= IDLE_INIT;
+    else
+        state <= next_state;
+
+//FSM_INIT
+always @(posedge sclk or negedge rst_n)
+    if(!rst_n)
+        next_state <= IDLE_INIT;
+    else
+        case(state)
+        IDLE_INIT:begin
+            if(SDRAM_flag == 1'b1)  next_state <= WAIT_INIT;
+            else    next_state <= IDLE_INIT;
+        end
+        WAIT_INIT:begin
+            
+        end
+
+        endcase
+
+always @(posedge sclk or negedge rst_n)
+    if(!rst_n)begin
+        ;
+    end
+    else
+        case(state)
+        IDLE_INIT:begin
+            ;
+        end
+        WAIT_INIT:begin
+            ;
+        end
+
+        default:begin
+            ;
+        end
+        endcase
+
 
 endmodule
